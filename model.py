66afe2b3226623c2bc8893cef515a1f7cbbf9731
harshavardhan784 <harshavardhan.govind@gmail.com>
2026-05-19 13:24:29 +0530

    added best snippet

--- model.py
+++ model.py
@@ -314,7 +314,7 @@ class Transformer(nn.Module):
 
     # UPDATE these after uploading your best checkpoint to GDrive
     # _GDRIVE_FILE_ID  = "1EHoboX_JUdHVC3HkLM4LTC-eMCBqvo1E"   # replace with new file id
-    _GDRIVE_FILE_ID = "1IzdfdxAy-o7qF4Kik5GYZHLRrOFfpaKs"
+    _GDRIVE_FILE_ID = "1yimsZCccSNZ-Z1fIUvqQ6GsnJLsmkPuQ"
     _CHECKPOINT_NAME = "checkpoint_epoch17.pt"                 # best val loss = 2.8796
 
     def __init__(

b4ba4a35460d0310dca146506fff2b0b707a6771
harshavardhan784 <harshavardhan.govind@gmail.com>
2026-05-19 08:11:42 +0530

    modified

--- model.py
+++ model.py
@@ -375,7 +375,8 @@ class Transformer(nn.Module):
     """
 
     # ── Replace with your actual Google Drive file ID after training ──
-    _GDRIVE_FILE_ID  = "1mwISmk_ySNmwG18s-tP10AHK_rGjjJEa"
+    # _GDRIVE_FILE_ID  = "1mwISmk_ySNmwG18s-tP10AHK_rGjjJEa" # 28.19
+    _GDRIVE_FILE_ID = "17GNhqcUo_F2jslvqUwDEP-CnhPkE8EeG"
     _CHECKPOINT_NAME = "baseline_best.pt"
 
     def __init__(
